50bd17ba8e87d5a87f285fc87f16ce207cd6383b
Miles Chick <mileschick@gmail.com>
2015-04-01 08:34:40 -0500

    Working on messages

--- application/views/contact/messages.php
+++ application/views/contact/messages.php
@@ -1,11 +1,28 @@
-<table>
-	<tr>
-		<th>YOUR MESSAGES</th>
+<table class='message_head'>
+	<tr class='message_head'>
+		<th colspan='7'>YOUR MESSAGES</th>
+	</tr>
+	<tr class='message_head'>
+		<td width='10%'>DATE & TIME</td>
+		<td width='10%'>EMAIL</td>
+		<td width='10%'>FIRST<br>NAME</td>
+		<td width='10%'>LAST<br>NAME</td>
+		<td width='10%'>PHONE</td>
+		<td width='40%'>MESSAGE</td>
+		<td width='10%'>DELETE</td>
 	</tr>
 </table>
-
-
-
-
-
-<?php print_r($messages); ?>
+<div class='message_table'>
+	<table>
+		<?php 
+			foreach ($messages as $key => $value) {
+				echo "<tr><td width='10%'>DATE & TIME</td>
+					<td width='10%'>EMAIL</td>
+					<td width='10%'>FIRST<br>NAME</td>
+					<td width='10%'>LAST<br>NAME</td>
+					<td width='10%'>PHONE</td>
+					<td width='40%'>MESSAGE</td>
+					<td width='10%'>DELETE</td></tr>";			}
+		 ?>
+	</table>
+</div>

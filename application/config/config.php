ff66e04af92ec405409a30a1d5d8994f2f505592
Miles Chick <mileschick@gmail.com>
2015-07-09 16:34:54 -0500

    Fix config baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://www.autumnwillow.bayrummedia.com';
+$config['base_url']	= 'http://www.bayrummedia.com/autumnwillow';
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
 
